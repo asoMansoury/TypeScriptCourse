fb5bb547c534cef8908e1386b4829c5b00543966
Aso <Rahman.Mansouri@cineplex.com>
2025-09-18 15:36:48 -0400

    add app.ts

--- Section 12/src/app.ts
+++ Section 12/src/app.ts
@@ -5,9 +5,21 @@ function Logger(logString:string = "Default Logger"){
     }
 }
 
+function withTemplate(template:string,hookId:string){
+    return function(constructor:any){
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor() as any;
+        if(hookEl){
+            hookEl.innerHTML = template;
+
+            hookEl.querySelector("h1")!.textContent = p.name;
+        }
+    }
+}
 
 
 @Logger("Hiii from decorator!")
+@withTemplate("<h1>My Person Object</h1>","app")
 class Person{
     name: string = "Max";
 
